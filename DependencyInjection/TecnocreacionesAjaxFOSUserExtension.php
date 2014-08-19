7f99b862b13a889c4b16607d680b88dc288560ca
Carlos Mendoza <inhack20@gmail.com>
2014-08-18 22:54:01 -0430

    Fix bug de compatibilidad con la version 2.0 de fos userbundle

--- DependencyInjection/TecnocreacionesAjaxFOSUserExtension.php
+++ DependencyInjection/TecnocreacionesAjaxFOSUserExtension.php
@@ -2,10 +2,12 @@
 
 namespace Tecnocreaciones\Bundle\AjaxFOSUserBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -25,5 +27,19 @@ class TecnocreacionesAjaxFOSUserExtension extends Extension
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
         
+        //Compatibilidad con fos user 1.3
+        if(!class_exists('FOS\UserBundle\FOSUserEvents')){
+            $registrationFormHandlerDefinition = new Definition("Tecnocreaciones\Bundle\AjaxFOSUserBundle\Handler\RegistrationFormHandler");
+            $registrationFormHandlerDefinition->setScope("request");
+            $registrationFormHandlerDefinition->setArguments(array(
+                new Reference("fos_user.registration.form"),
+                new Reference("request"),
+                new Reference("fos_user.user_manager"),
+                new Reference("fos_user.mailer"),
+                new Reference("fos_user.util.token_generator"),
+                new Reference("event_dispatcher"),
+            ));
+            $container->setDefinition("fos_user.registration.form.handler.default", $registrationFormHandlerDefinition);
+        }
     }
 }
